6ac04e77114dc6022d84a2c9c5eec79e15507302
Eva Malluta <eva.malluta14@gmail.com>
2022-07-28 10:17:05 +0200

    change

--- src/components/EmailList.tsx
+++ src/components/EmailList.tsx
@@ -1,31 +1,12 @@
+import { SingleEmail } from "./SingleEmail";
+
 export function EmailList({getFilteredEmails,toggleRead, toggleStar}){
     return(
         <main className="emails">
       <ul>
           {getFilteredEmails().map((email, index) => (
-            <li
-              key={index}
-              className={`email ${email.read ? 'read' : 'unread'}`}
-            >
-              <div className="select">
-                <input
-                  className="select-checkbox"
-                  type="checkbox"
-                  checked={email.read}
-                  onChange={() => toggleRead(email)}
-                />
-              </div>
-              <div className="star">
-                <input
-                  className="star-checkbox"
-                  type="checkbox"
-                  checked={email.starred}
-                  onChange={() => toggleStar(email)}
-                />
-              </div>
-              <div className="sender">{email.sender}</div>
-              <div className="title">{email.title}</div>
-            </li>
+            <SingleEmail
+            index={index} email={email} toggleRead={toggleRead} toggleStar={toggleStar}/>
           ))}
         </ul>
       </main>
